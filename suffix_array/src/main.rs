fbd0a83d119e7d454b14ebe792fb73f24aafc178
Sebastian C <sebastian9111@gmail.com>
2022-01-19 19:11:36 +0200

    mod suffix_array/step2

--- suffix_array/src/main.rs
+++ suffix_array/src/main.rs
@@ -1,6 +1,27 @@
-// use std::env::args;
 use std::io;
-// use std::slice;
+
+fn count_sort(p: &Vec<usize>, c: &Vec<usize>) -> Vec<usize> {
+    let n = p.len();
+    let mut cnt: Vec<usize> = vec![0; n];
+    for x in c.clone() {
+        cnt[x] += 1;
+    }
+
+    let mut p_new: Vec<usize> = vec![0; n];
+    let mut pos: Vec<usize> = vec![0; n];
+    // pos[0] = 0;
+    for i in 1..n {
+        pos[i] = pos[i - 1] + cnt[i - 1];
+    }
+
+    for x in p.clone() {
+        let i = c[x];
+        p_new[pos[i]] = x;
+        pos[i] += 1;
+    }
+
+    p_new
+}
 
 fn main() {
     // let args: Vec<String> = args().collect();
@@ -20,8 +41,8 @@ fn main() {
     let mut a: Vec<(char, usize)> = vec![('$', 0); n];
 
     let mut idx = 0;
-    for c in s.chars() {
-        a[idx] = (c, idx);
+    for ch in s.chars() {
+        a[idx] = (ch, idx);
         idx += 1;
     }
 
@@ -31,7 +52,7 @@ fn main() {
         p[i] = a[i].1;
     }
 
-    c[p[0]] = 0;
+    // c[p[0]] = 0;
 
     for i in 1..n {
         if a[i].0 == a[i - 1].0 {
@@ -45,33 +66,33 @@ fn main() {
 
     while (1 << k) < n {
         //k -> k+1
-        let mut a: Vec<((usize, usize), usize)> = vec![((0, 0), 0); n];
 
         for i in 0..n {
-            a[i] = ((c[i], c[(i + (1 << k)) % n]), i);
-        }
-        a.sort_by_key(|k| k.0);
-
-        for i in 0..n {
-            p[i] = a[i].1;
+            let tmp: i32 = (p[i] as i32) - (1 << k);
+            let tmp2 = (tmp + (n as i32)) as usize;
+            p[i] = tmp2 % n;
         }
 
-        c[p[0]] = 0;
+        // let a = radix_sort(a);
+        p = count_sort(&p, &c);
+
+        let mut c_new: Vec<usize> = vec![0; n];
+        // c_new[p[0]] = 0;
 
         for i in 1..n {
-            if a[i].0 == a[i - 1].0 {
-                c[p[i]] = c[p[i - 1]];
+            let prev: (usize, usize) = (c[p[i - 1]], c[(p[i - 1] + (1 << k)) % n]);
+            let now: (usize, usize) = (c[p[i]], c[(p[i] + (1 << k)) % n]);
+            if now == prev {
+                c_new[p[i]] = c_new[p[i - 1]];
             } else {
-                c[p[i]] = c[p[i - 1]] + 1;
+                c_new[p[i]] = c_new[p[i - 1]] + 1;
             }
         }
-
+        c = c_new;
         k += 1;
     }
 
     for i in p {
         print!("{} ", i);
     }
-
-    // println!("{:?}", p);
 }
